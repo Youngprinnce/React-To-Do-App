10360e7701789217013a2f7e3a354e647db64e76
youngprinnce <ajiboyeadedotun16@gmail.com>
2020-09-26 04:18:18 +0100

    added HTML 5 localStorage to persist Todo List

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import todosData from "./todosData";
 import Todo from "./Todo"
 import './App.css';
 
 const App = () => {
-  const [todoLists, setTodoLists] = useState(todosData);
+  const [todoLists, setTodoLists] = useState([]);
+
+  //You can choose to view built in To Do data by uncommenting the state below and commening the on above
+  // const [todoLists, setTodoLists] = useState(todosData);
   const [search, setSearch] = useState("");
+
+  useEffect(()=>{
+    getLocalTodos()
+  }, [])
+
+   useEffect(()=>{
+    saveLocalTodos()
+  }, [todoLists])
 
   const updateSearch = (e) => {
     setSearch(e.target.value);
@@ -15,6 +26,19 @@ const App = () => {
     setTodoLists([...todoLists, { text: search, id: todoLists.length + 1, completed: false }])
     setSearch("");
     return todoLists
+  }
+
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todoLists))
+  };
+
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]))
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem("todos"))
+      setTodoLists(todoLocal);
+    }
   }
 
   return (
